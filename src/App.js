734db016b081aa7f8df30db7cf4c6b664c28f574
Joseph bayad <jobayad66@gmail.com>
2020-08-17 17:40:25 +0100

    sub header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
+import Subheader from './components/Subheader';
 import './index.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Subheader />
       <Router>
         <div className="container">
           <Switch>
---
+++ src/components/Subheader/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+const SubHeader = () => {
+  return (
+    <div className="subheader">
+      <p>
+        Free shipping when your order is above €99.99 | 2 Year Limited Warranty.
+      </p>
+    </div>
+  );
+};
+
+export default SubHeader;
